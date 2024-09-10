cadf5b4f7e3698cbdf6ef5b2365f89ded18e4ca5
pdu08075 <rpaeheh@naver.com>
2024-09-10 12:50:55 +0900

    2024.09.10.12

--- src/main/java/com/korit/senicare/provider/JwtProvider.java
+++ src/main/java/com/korit/senicare/provider/JwtProvider.java
@@ -1,8 +1,11 @@
 package com.korit.senicare.provider;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 import java.util.Date;
@@ -35,11 +38,48 @@ public class JwtProvider {
         try {
             // JWT 암호화에 사용할 Key 생성
             Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));      // 바이트로 변형
+
+            // jwt 생성
+            jwt = Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(expiredDate)
+                .compact();     // 압축, 생성
+
         } catch (Exception exception) {
             exception.printStackTrace();
             return null;
         }
         return jwt;
     }
+    
+    
+    // 검증 메서드
+    public String validate(String jwt) {
+
+        String userId = null;
+
+        try {
+
+            // JWT 암호화에 사용할 Key 생성
+            Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+            // jwt 검증 및 payload의 subject 값 추출
+            userId = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jwt)        // .parseClaimsJws(jwt).getBody()로 파싱 작업 및 클레임, 즉 페이로드를 꺼내오는 작업 수행
+                .getBody()
+                .getSubject();
+
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+        return userId;
+    }
+
 
 }
