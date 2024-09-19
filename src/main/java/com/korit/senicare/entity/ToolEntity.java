a723c819b6d0c257717d053b020102166528a7fb
pdu08075 <rpaeheh@naver.com>
2024-09-19 16:51:13 +0900

    2024.09.19.16

--- src/main/java/com/korit/senicare/entity/ToolEntity.java
+++ src/main/java/com/korit/senicare/entity/ToolEntity.java
@@ -1,5 +1,7 @@
 package com.korit.senicare.entity;
 
+import com.korit.senicare.dto.request.tool.PostToolRequestDto;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,10 +21,15 @@ import lombok.Setter;
 public class ToolEntity {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // toolNumber는 자동 생성이라 직접 값을 넣지 않고 초기에 null 값을 유지하도록 함
     private Integer toolNumber;
     private String name;
     private String purpose;
     private Integer count;
     
+    public ToolEntity(PostToolRequestDto dto) {
+        this.name = dto.getName();
+        this.purpose = dto.getPurpose();
+        this.count = dto.getCount();
+    }
 }
