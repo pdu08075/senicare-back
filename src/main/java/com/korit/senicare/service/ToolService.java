235a37d17ef77f5f5bd88850501f785c106d14a5
pdu08075 <rpaeheh@naver.com>
2024-09-20 10:12:13 +0900

    2024.09.20.09

--- src/main/java/com/korit/senicare/service/ToolService.java
+++ src/main/java/com/korit/senicare/service/ToolService.java
@@ -3,8 +3,10 @@ package com.korit.senicare.service;
 import org.springframework.http.ResponseEntity;
 
 import com.korit.senicare.dto.request.tool.PostToolRequestDto;
+import com.korit.senicare.dto.response.tool.GetToolListResponseDto;
 import com.korit.senicare.dto.response.ResponseDto;
 
 public interface ToolService {
     ResponseEntity<ResponseDto> postTool(PostToolRequestDto dto);
+    ResponseEntity<? super GetToolListResponseDto> getToolList();
 }
